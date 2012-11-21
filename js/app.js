d32f73466bb873dbf0d49905fd56de92e9dd098e
leandro-romero <leandro.s.romero@gmail.com>
2012-11-20 23:10:13 -0300

    Image optimization.
    Improve in the view cake_list.html, galery/list mode added.

--- js/app.js
+++ js/app.js
@@ -45,3 +45,29 @@ cake_shop.controller('NavCtrl', ['$scope', '$location', function ($scope, $locat
         return page == currentRoute ? 'active' : '';
     };
 }]);
+
+cake_shop.filter('grouped', function() {
+  return function(input, itemsPerRow) {
+    if (itemsPerRow === undefined) {
+      itemsPerRow = 1;
+    }
+
+    var out = [];
+    for (var i = 0; i < input.length; i++) {
+
+      var rowElementIndex = i % itemsPerRow;
+      var rowIndex = (i - rowElementIndex) / itemsPerRow;
+      var row;
+      if (rowElementIndex === 0) {
+        row = [];
+        out[rowIndex] = row;
+      } else {
+        row = out[rowIndex];
+      }
+
+      row[rowElementIndex] = input[i];
+    }
+
+    return out;
+  };
+});
